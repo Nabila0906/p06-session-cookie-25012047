acd3f0fff820cf2b7b37cf5dd9835be3aabdd8a2
Nabila <akunnabila09062007@gmail.com>
2026-09-17 12:43:17 +0700

    feast: tambahkan dataset dan fungsi bantuan

---
+++ data/products.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    1 => ['nama' => 'Kopi Susu', 'harga' => 15000],
+    2 => ['nama' => 'Roti Cokelat', 'harga' => 12000],
+    3 => ['nama' => 'Teh Lemon', 'harga' => 10000],
+];
---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+function e(string $value): string
+{
+    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+}
+
+function setFlash(string $message): void
+{
+    $_SESSION['flash'] = $message;
+}
+
+function pullFlash(): ?string
+{
+    $message = $_SESSION['flash'] ?? null;
+    unset($_SESSION['flash']);
+    return is_string($message) ? $message : null;
+}
+
+function cartCount(array $cart): int
+{
+    return array_sum(array_map('intval', $cart));
+}
